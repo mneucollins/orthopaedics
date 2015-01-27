4d01d401c99e9f72892a0ff9dbeeb9eb79cad904
camilo <camilo.rodriguez@imbassolutions.com>
2015-01-27 09:46:15 -0500

    loading the data inside the grid, evaluating interval times, interface design in schedule, added collapsible panel

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -53,6 +53,9 @@ orthopaedicsControllers.controller('scheduleCtrl', ['$scope', '$location', '$roo
     });
 
     $scope.getNormalizedHour = function (hour) {
+      var d = new Date(hour);
+      hour = d.getHours();
+      
       if(hour == 12)
         return hour + " M";
       else if(hour < 12)
@@ -104,6 +107,102 @@ orthopaedicsControllers.controller('scheduleCtrl', ['$scope', '$location', '$roo
 
       return classes;
     }
+
+    $scope.getImagingState = function (patient){
+      var imagingStateIcon = "";
+
+      if(patient.needsImaging){
+        if(patient.imagingTimestamp){
+          imagingStateIcon = "img/ok1icon.png";
+        }
+        else{
+          imagingStateIcon = "img/yicon.png";
+        }
+      }
+      else{
+        imagingStateIcon = "img/nicon.png";
+      }
+
+      return imagingStateIcon;
+    }
+
+    $scope.getCurrentStatus = function (patient){
+      var current_Status = "";
+      
+      if(patient.currentState == "NCI"){
+        current_Status = "WR 0min - EX 0min"
+      }
+      else if(patient.currentState == "WR"){
+        var wrDate = new Date(patient.WRTimestamp);
+        var nowDate = new Date();
+        var minutes = Math.round((nowDate - wrDate) / (60*1000));
+        
+        current_Status = "WR " + minutes + "min - EX 0min";
+      }
+      else if(patient.currentState == "EX"){
+        var wrDate = new Date(patient.WRTimestamp);
+        var exDate = new Date(patient.EXTimestamp);
+        var nowDate = new Date();
+
+        var minutesWR = Math.round((exDate - wrDate) / (60*1000));
+        var minutesEX = Math.round((nowDate - exDate) / (60*1000));
+
+        current_Status = "WR " + minutesWR + "min - EX " + minutesEX + "min";
+      }
+      else if(patient.currentState == "DC"){
+        var wrDate = new Date(patient.WRTimestamp);
+        var exDate = new Date(patient.EXTimestamp);
+        var dcDate = new Date(patient.DCTimestamp);
+
+        var minutesWR = Math.round((exDate - wrDate) / (60*1000));
+        var minutesEX = Math.round((dcDate - exDate) / (60*1000));
+
+        current_Status = "WR " + minutesWR + "min - EX " + minutesEX + "min";
+      }
+
+      return current_Status;
+    }
+
+     $scope.getTotalTime = function (patient){
+      var totalTime = "aa";
+      var wrDate = new Date(patient.WRTimestamp);
+      var exDate = new Date(patient.EXTimestamp);
+      var dcDate = new Date(patient.DCTimestamp);
+
+      if(wrDate != "Invalid Date"){
+        if(exDate != "Invalid Date"){
+          if(dcDate != "Invalid Date"){
+            var wrDate = new Date(patient.WRTimestamp);
+            var exDate = new Date(patient.EXTimestamp);
+            var dcDate = new Date(patient.DCTimestamp);
+
+            var minutesWR = Math.round((exDate - wrDate) / (60*1000));
+            var minutesEX = Math.round((dcDate - exDate) / (60*1000));
+            totalTime = minutesWR + minutesEX + " min";
+          }
+          else{
+            var wrDate = new Date(patient.WRTimestamp);
+            var exDate = new Date(patient.EXTimestamp);
+            var nowDate = new Date();
+            var minutesWR = Math.round((exDate - wrDate) / (60*1000));
+            var minutesEX = Math.round((nowDate - exDate) / (60*1000));
+            totalTime = minutesWR + minutesEX + " min";
+          }
+        }
+        else{
+          var wrDate = new Date(patient.WRTimestamp);
+          var nowDate = new Date();
+          var minutes = Math.round((nowDate - wrDate) / (60*1000));
+          totalTime = minutes + " min";
+        }
+      }
+      else
+      {
+        totalTime = "0 min";
+      }
+
+      return totalTime;
+     }
 
   }]);
 
