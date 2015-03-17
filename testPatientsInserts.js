dd8d753ce383e6fddfc1dd14c353c012da0d05a7
Ezael Bello <ezabaw@gmail.com>
2015-03-17 13:30:03 -0500

    changes over test patients

--- testPatientsInserts.js
+++ testPatientsInserts.js
@@ -7,7 +7,7 @@ var config = require("./config.json");
 
 mongoose.connect(config.databaseURL);
 
-var nPlusHours = 4;
+var nPlusHours = 3;
 
 var testPatients = [{
 	firstName: "Tom",
@@ -241,7 +241,7 @@ var testPatients = [{
 
 physicianController.listarPhysicians(function (err, physicians) {
 	for (var i = 0; i < testPatients.length; i++) {
-		var index = parseInt(Math.random() * 100) % physicians.length;
+		var index = parseInt(Math.random() * 100) % 5 //physicians.length;
 		testPatients[i].physician = physicians[index].id;
 		patientController.nuevoPatient(testPatients[i], function (err, data) {
 		    if(err) console.log(err);
