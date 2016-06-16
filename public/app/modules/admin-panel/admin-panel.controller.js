8b831f0d3e1aa93af88007b3e84357396d486ffb
camilo <camilo.rodriguez@imbassolutions.com>
2016-06-16 17:26:15 -0500

    implementation of fuse.js to find users and load de results in individual divs.

--- public/app/modules/admin-panel/admin-panel.controller.js
+++ public/app/modules/admin-panel/admin-panel.controller.js
@@ -3,54 +3,27 @@ angular.module('adminModule')
 .controller('adminCtrl', ['$scope', '$location', '$rootScope', '$log', 'User',
   function($scope, $location, $rootScope, $log, User) {
 
-    $scope.findUser = "hola";
+    //$scope.findUser = "b";
 
-    var users = User.query(function() {});
-    // User.setSecurityQuestions({userId: theUser._id}, {
-    //     email: $scope.user.email, 
-    //     securityQuestion: $scope.user.question, 
-    //     securityAnswer: $scope.user.answer
-    // }, function(user) {
-    //     if(!user) {
-    //         Alerts.addAlert("warning", "There was an error saving the user");
-    //         return;
-    //     }
-        
-    //     goToDashboard(user);
-
-    // }, function (err) {
-    //     Alerts.addAlert("danger", "ups! we got an error: " + JSON.stringify(err));
-    // });
-
-
-    var books = [{
-  id: 1,
-  title: 'The Great Gatsby',
-  author: 'F. Scott Fitzgerald'
-},{
-  id: 2,
-  title: 'The DaVinci Code',
-  author: 'Dan Brown'
-},{
-  id: 3,
-  title: 'Angels & Demons',
-  author: 'Dan Brown'
-}];
+// var options = {
+//   keys: ['author', 'title'],   // keys to search in
+//   id: 'id'                     // return a list of identifiers only
+// }
 
 var options = {
-  keys: ['author', 'title'],   // keys to search in
-  id: 'id'                     // return a list of identifiers only
+  keys: ['name'],   // keys to search in
+  id: 'name'                     // return a list of identifiers only
 }
 
-    var fuse = new Fuse(books, options);
+    //var fuse = new Fuse(books, options);
+    var fuse;// = new Fuse($scope.usersArray, options);
+
+    User.query(function(data, err) {
+      fuse = new Fuse(data, options);
+    });
 
     $scope.search = function (findUser) {
-        var result = fuse.search(findUser);
-        alert(result);
-        //$result.empty();
-      // $.each(r, function() {
-      //   $result.append('<li class="result-item">' + this.title + ', <span>' + this.author + '</span></li>');
-      // });
+        $scope.result = fuse.search(findUser);
     }
 
     function retrievePatients () {
