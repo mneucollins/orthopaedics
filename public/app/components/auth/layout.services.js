53286e378152c96a9b977d5851c706e0e2ae2e22
Ezael Bello <ezabaw@gmail.com>
2016-06-22 18:37:43 -0500

    New organization for layouts.

--- public/app/components/auth/layout.services.js
+++ public/app/components/auth/layout.services.js
@@ -1,24 +1,38 @@
 angular.module("appCommons")
 .factory('LayoutService',['AuthService',function(AuthService){
 
-	var currentUser = AuthService.currentUser();
+	var layout;
+	if(AuthService.currentUser())
+		if(AuthService.currentUser().isCustomLayout){
+			layout = AuthService.currentUser().role.layout;
+		} else {
+			layout = AuthService.currentUser().layout;
+		}
 
 	return {
-
-		coloredPriorTime : function(){
-			if(currentUser.isCustomLayout){
-				return currentUser.role.layout.coloredPriorTime;
+		setLayoutUser: function (user) {
+			if(AuthService.currentUser().isCustomLayout){
+				layout = user.role.layout;
 			} else {
-				return currentUser.layout.coloredPriorTime;
+				layout = user.layout;
 			}
+		}
+		coloredPriorTime : function(){
+			// if(currentUser.isCustomLayout){
+			// 	return currentUser.role.layout.coloredPriorTime;
+			// } else {
+			// 	return currentUser.layout.coloredPriorTime;
+			// }
+			return layout.coloredPriorTime;
 		},
 
 		highlightNewPatients : function(){
-			if(currentUser.isCustomLayout){
-				return currentUser.role.layout.highlightNewPatients;
-			} else {
-				return currentUser.layout.highlightNewPatients;
-			}
+			// if(currentUser.isCustomLayout){
+			// 	return currentUser.role.layout.highlightNewPatients;
+			// } else {
+			// 	return currentUser.layout.highlightNewPatients;
+			// }
+			return layout.highlightNewPatients;
 		}
 
 	};
