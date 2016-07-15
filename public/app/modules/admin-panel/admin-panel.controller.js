062fc2bcd795edb05fdc8a66569839490c3435c6
camilo <camilo.rodriguez@imbassolutions.com>
2016-07-15 15:21:12 -0500

    Added form content of role tab in admin panel. Added role-config directive, and integration with DB. Revision in role Drag pending.

--- public/app/modules/admin-panel/admin-panel.controller.js
+++ public/app/modules/admin-panel/admin-panel.controller.js
@@ -1,22 +1,31 @@
 
 angular.module('adminModule')
-.controller('adminCtrl', ['$scope', '$location', '$rootScope', '$log', 'User', 'Physician','Alerts','Role',
-  function($scope, $location, $rootScope, $log, User, Physician, Alerts, Role) {
+.controller('adminCtrl', ['$scope', '$location', '$rootScope', '$log', 'User', 'Physician','Alerts','Role', 'LayoutService',
+  function($scope, $location, $rootScope, $log, User, Physician, Alerts, Role, LayoutService) {
 
     var fusePhysicians;
     var options;
+    var inRoles = false;
     $scope.newUser = false;
     $scope.selectedItem = null;
+    $scope.items1 = [];
+    $scope.items2 = [];
 
     $scope.loadUsers = function(){
         $scope.result = "";
         $scope.findUser = "";
+        inRoles = false;
     }
     
     $scope.loadPhysicians = function(){
         $scope.result = "";
         $scope.findUser = "";
         $scope.selectedItem = null;
+        inRoles = false;
+    }
+
+    $scope.loadRoles = function (){
+        inRoles = true;
     }
 
     $scope.fuseUsers;// = new Fuse($scope.usersArray, options);
@@ -35,6 +44,15 @@ angular.module('adminModule')
 
     $scope.$on('listado', function(event, args){
         $scope.selectedItem = args.listado;
+        if(inRoles)
+        {
+            $scope.items1 = [];
+            $scope.items2 = [];
+            $scope.layout = LayoutService.getLayoutUser();
+            $scope.items2 = LayoutService.getActiveColumns($scope.selectedItem.layout);
+            $scope.items1 = LayoutService.getInactiveColumns($scope.selectedItem.layout);
+        }
+        
         if(jQuery.isEmptyObject(args.listado) )
             $scope.newUser = true;
         else
---
+++ public/app/modules/admin-panel/role-config.directive.js
@@ -0,0 +1,43 @@
+angular.module('adminModule')
+.directive('roleConfig',function(){
+	return {
+		replace : true,
+		restrict : 'E',
+		templateUrl : '/app/modules/admin-panel/role-config.html',
+		controller:['$scope', '$rootScope', '$modal', '$log', 'Config', 'Alerts', 'User',
+		function($scope, $rootScope, $modal, $log, Config, Alerts, User) {
+
+			// $scope.items1 = [];
+			// $scope.items2 = [];
+
+			$scope.saveRoleChanges = function () {
+				var selectedRole = $scope.selectedItem.role;
+		        if($scope.newUser == true)
+		        {
+		            User.save($scope.selectedItem, 
+		               function (argument) {
+		               Alerts.addAlert("success", "User created!");
+		               $scope.selectedItem.role = selectedRole;
+		               $scope.newUser = false;
+		            }, function (err) {
+		                Alerts.addAlert("warning", "Error");
+		            });
+		        }
+		        else
+		        {
+		            $scope.selectedItem.role = $scope.selectedItem.role._id;
+		            User.update({userId: $scope.selectedItem._id}, 
+		                $scope.selectedItem, 
+		                function (argument) {
+			               Alerts.addAlert("success", "User updated!");
+			               $scope.selectedItem.role = selectedRole;
+			               $scope.newUser = false;
+		            }, function (err) {
+		                Alerts.addAlert("warning", "Error");
+		            });
+		        }
+		    }
+
+		}]
+	}
+});
