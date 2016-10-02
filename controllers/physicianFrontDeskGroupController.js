aa94050d1c116281a8a0de22046e3f0dbc6f34b1
Ezael Bello <ezabaw@gmail.com>
2016-10-02 08:25:04 -0500

    selected physician calculation service

--- controllers/physicianFrontDeskGroupController.js
+++ controllers/physicianFrontDeskGroupController.js
@@ -12,6 +12,7 @@ module.exports = {
     obtenerPhysicianFrontDeskGroup: obtenerPhysicianFrontDeskGroup,
     actualizarPhysicianFrontDeskGroup: actualizarPhysicianFrontDeskGroup,
     eliminarPhysicianFrontDeskGroup: eliminarPhysicianFrontDeskGroup,
+    getPhysicianListMetrics: getPhysicianListMetrics,
     getGroupMetrics: getGroupMetrics
 }
 
@@ -73,69 +74,66 @@ function eliminarPhysicianFrontDeskGroup(id, callback) {
     });
 }
 
+function getPhysicianListMetrics(physicianIdArray, callback) {
+    
+    var results = {
+        sumMinutes: 0,
+        numPatients: 0,
+        threshold: -500
+    };
+
+    physicianController
+    .getClinicDelay(physicianIdArray, function (err, clinicDelays) {
+        if(err) return callback(err);
+
+        patientController
+        .listPatientsbyPhysicianListByStateToday(physicianIdArray, 'PR', 
+        function (err, patients) {
+            if(err) return callback(err);
+
+            _.each(patients, function (pat, i, lst) {
+               
+                var prTime = tools.getPRTime(pat);
+                var appt = moment(pat.apptTime);
+                var clinicDelay = clinicDelays[pat.physician._id];
+                
+                var threshold = moment().diff(appt.add(clinicDelay, "minutes"), "minutes");
+
+                results.sumMinutes += prTime;
+                results.numPatients++;
+                if(threshold > results.threshold)
+                    results.threshold  = threshold;
+            });
+
+            return callback(null, results);
+        });
+    });
+}
+
 function getGroupMetrics (physicianFrontDeskGroupIdArray, callback) {
     
     var results = {};
-    // console.log("entra a getGroupMetrics");
+
     listarPhysicianFrontDeskGroupsByIdList(physicianFrontDeskGroupIdArray, function (err, physicianGroupList) {
         if(err) return callback(err);
         var phyGroupCounter = 0;
-        // console.log("objetos consultados! comenzando recorrer");
+
         if(physicianGroupList.length == 0 ) 
             return callback(null, results);
 
         _.each(physicianGroupList, function (phyGroup, i, lst) {
 
-            results[phyGroup._id] = {
-                sumMinutes: 0,
-                numPatients: 0,
-                threshold: -500
-            };
-
-            physicianController
-            .getClinicDelay(phyGroup.physicians, function (err, clinicDelays) {
+            getPhysicianListMetrics(phyGroup.physicians, function (err, result) {
                 if(err) return callback(err);
-                // console.log("clinic delay listado para grupo " + phyGroupCounter);
-                // console.log("phyGroup.physicians " + JSON.stringify(phyGroup.physicians));
 
-                patientController
-                .listPatientsbyPhysicianListByStateToday(phyGroup.physicians, 'PR', 
-                function (err, patients) {
-                    if(err) return callback(err);
+                results[phyGroup._id] = result;     
 
-                    // console.log("Patients listados para grupo " + phyGroupCounter + " total patients: " + patients.length);
-                    _.each(patients, function (pat, i, lst) {
-                        // console.log("i: " + i);
-                        var prTime = tools.getPRTime(pat);
-                        var appt = moment(pat.apptTime);
-                        var clinicDelay = clinicDelays[pat.physician._id];
-                        // var prTimestamp = moment(pat.PRTimestamp);
-                        // var prDelay = (new Date().getTime() - new Date(pat.PRTimestamp).getTime()) / (60 * 1000);
-                        // console.log("Init ");
-                        // var threshold = prTimestamp.add(prDelay, "minutes").dif...
-                        var threshold = moment().diff(appt.add(clinicDelay, "minutes"), "minutes");
-                        // console.log("Calc ");
-
-                        results[phyGroup._id].sumMinutes += prTime;
-                        results[phyGroup._id].numPatients++;
-                        if(threshold > results[phyGroup._id].threshold)
-                            results[phyGroup._id].threshold  = threshold;
-
-
-                        console.log("Patient " + i + ": " + 
-                            results[phyGroup._id].sumMinutes +  "-" + 
-                            results[phyGroup._id].numPatients +  "-" + 
-                            results[phyGroup._id].threshold);
-                    });
-
-                    phyGroupCounter++;
-                    console.log("counter: " + phyGroupCounter);
-                    if(phyGroupCounter >= physicianGroupList.length) {
-                        console.log("calling back!!");
-                        return callback(null, results);
-                    }
-
-                });
+                phyGroupCounter++;
+                // console.log("counter: " + phyGroupCounter);
+                if(phyGroupCounter >= physicianGroupList.length) {
+                    console.log("calling back!!");
+                    return callback(null, results);
+                }
             });
         });
     });
